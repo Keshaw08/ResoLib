3ff85d7cdaad5c63c90d8b74da6475571071b06f
Keshaw Soni <63107199+Keshaw08@users.noreply.github.com>
2022-04-24 19:27:40 +0530

    Add files via upload

---
+++ CS1106 Project/Project/Account/account.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <meta content='maximum-scale=1.0, initial-scale=1.0, width=device-width' name='viewport'>
+
+  <!-- Google Font Signika -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Signika&display=swap" rel="stylesheet">
+
+  <!-- BootStrap CSS and JS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  <!-- Style Sheet -->
+  <link rel="stylesheet" href="../css/styles.css">
+  <title>Home</title>
+</head>
+<body>
+  <div class="container-fluid">
+
+    <!-- Header and Navbar -->
+    <div class="header">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <!-- <a class="navbar-brand" href="#">Home</a> -->
+          <img class="logo-img" src="../images/logo-test.png" alt="logo">
+          <!-- <img class="logo-img" src="images/logo1.png" alt="logo"> -->
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="../index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Account</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../../upload-testing/Upload/display_pdf.php">Display</a>
+              </li>
+            </ul>
+          </div>
+            
+          <div class="nav-upload">
+            <!-- <form class="form-inline my-2 my-lg-0 ml-auto">
+              <input class="form-control" type="search" placeholder="Search" aria-label="Search"><button class="btn btn-outline-white btn-md my-2 my-sm-0 ml-3" type="submit">Search</button>
+              
+            </form> -->
+
+             <a class="btn btn btn-sm" type="button" name="button" href="../upload-testing/Upload/insert.php"class="nav-link">UPLOAD</a>
+          </div>
+          
+        </div>
+      </nav>
+    </div>
+
+
+  </div>
+
+
+
+</body>
+
+</html>
